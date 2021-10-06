a755a8112067535ddf2db48674c9c1afbc46b9b9
crazypeter74 <felix.renken@yahoo.de>
2021-10-06 12:07:22 +0200

    added animation

--- myscripts.js
+++ myscripts.js
@@ -1,8 +1,12 @@
+
+const swup = new Swup()
+
 function vanish() {
     document.getElementById("btn-1").style.opacity = "0";
     
 }
 
 function photography() {
-    location.href='photography.html';
+    location.href='/photography.html';
 }
+
