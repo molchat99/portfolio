8e933beea224cdce6935dd1be53769beffab5741
crazypeter74 <felix.renken@yahoo.de>
2021-10-08 13:54:48 +0200

    changed

--- myscripts.js
+++ myscripts.js
@@ -1,12 +1,8 @@
-
 const swup = new Swup()
 
-function vanish() {
-    document.getElementById("btn-1").style.opacity = "0";
-    
-}
+new kursor({
+    type: 1,
+    removeDefaultCursor: true,
+    color: "#efc102",
 
-function photography() {
-    location.href='/photography.html';
-}
-
+});
